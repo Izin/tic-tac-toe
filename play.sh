8a8b316f02a8007fe6ea2d21095f614a3fc84821
Izin <git@dgjvhdym.xyz>
2019-09-30 19:29:14 +0200

    test

--- play.sh
+++ play.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-###############################################################################
-## A simple ASCII TicTacToe game made with bash
-## @author  Malo Blanchard
-## @email   github@maloblanchard.com
-## @version 2.0.0
-###############################################################################
+
+# Name:         TicTacToe
+# Description:  A simple ASCII Tic Tac Toe game made
+# Author:       Malo Blanchard
+# version:      2.0.0
+
+set -eu
+
+#@todo replace ttt. by ttt- (prefix for functions)
+#@todo very function with a space before ()
+# @todo functions comments with single '#'
 
 source ./src/utils.sh
 source ./src/back.sh
